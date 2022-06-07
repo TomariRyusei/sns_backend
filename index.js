6a79929fac4c8d703de5abb87558e14283d52b5f
TomariRyusei <t.ryu6635@gmail.com>
2022-06-07 23:12:44 +0900

    Setting up server

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app = express();
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`listening at ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
