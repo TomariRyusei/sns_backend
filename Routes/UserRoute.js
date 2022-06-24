22280f7a44dcbe7c4cbb2d81ce42bb4d2b87af65
TomariRyusei <t.ryu6635@gmail.com>
2022-06-24 15:44:26 +0900

    Setting up JWT auth

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -8,14 +8,15 @@ import {
   followUser,
   UnFollowUser,
 } from "../Controllers/UserController.js";
+import authMiddleWare from "../middleware/AuthMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", getAllUsers);
 router.get("/:id", getUser);
-router.post("/:id", updateUser);
-router.delete("/:id", deleteUser);
-router.put("/:id/follow", followUser);
-router.put("/:id/unfollow", UnFollowUser);
+router.post("/:id", authMiddleWare, updateUser);
+router.delete("/:id", authMiddleWare, deleteUser);
+router.put("/:id/follow", authMiddleWare, followUser);
+router.put("/:id/unfollow", authMiddleWare, UnFollowUser);
 
 export default router;
